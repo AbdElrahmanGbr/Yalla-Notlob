bda886953b920287b0c8f31277f505900d5fe9ea
omaimataha2 <omaimamahmoud244@gmail.com>
2022-05-23 18:51:26 +0200

    add user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,4 +22,13 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def add_user
+    @groups = current_user.groups.all
+    @selected_group_id = params[:group_id]
+    @selected_group = Group.find @selected_group_id
+    @members = Group.find(@selected_group_id).group_participants.all.map { |group_participant| group_participant.user }
+    @member = current_user.groups.find(@selected_group_id).group_participants.new
+    @group = Group.new
+  end
+
 end
