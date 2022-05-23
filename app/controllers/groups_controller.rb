1d0faa35f554f968b7622f9f47009178d896b109
omaimataha2 <omaimamahmoud244@gmail.com>
2022-05-23 18:50:12 +0200

    destroy group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,5 +16,10 @@ class GroupsController < ApplicationController
     end
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path
+  end
 
 end
