549c151068058f5f5c1ac2d9eab273a407b49db7
Mohamed-Shhata <mohamed01068215253@gmail.com>
2022-05-24 00:53:01 +0200

    add member order to view

--- app/controllers/order_members_controller.rb
+++ app/controllers/order_members_controller.rb
@@ -1,8 +1,12 @@
 class OrderMembersController < ApplicationController
 
     def index
-        @orders_members = OrderMember.where(user_id: current_user.id)
-      end
+        @order = Order.find(params[:id])
+        @items = OrderMember.where(order: @order)
+    end
+        
+        # @orders_members = OrderMember.where(user_id: current_user.id)
+
     
       def show
       end
@@ -21,5 +25,18 @@ class OrderMembersController < ApplicationController
           format.js { render partial: "javascripts/order_members/create_order_member" }
         end
       end
+
+        
+  def destroy
+    @order = Order.find(params[:order_id])
+    @order_members = @order.order_members.find(params[:id])
+    @order_members.destroy
+    puts(params)
+    respond_to do |format|
+      format.js { render partial: "javascripts/orders/destroy_order_member" }
+    end
+  end
+
+        
 
 end
