c432dbab0b7db9082860e5b309bd51bff965ab2f
Eid Azzam <eidmahmoud134@gmail.com>
2022-05-28 01:26:20 +0200

    adding devise routes reg (views)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  authenticated :user do
+    root "pages#home", as: "authenticated_root"
+  end
+  devise_scope :user do
+    root "devise/sessions#new"
+  end
   resources :orders
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "orders#index"
-  # root 'pages#myaccount'
+  # root "orders#index"
 end
